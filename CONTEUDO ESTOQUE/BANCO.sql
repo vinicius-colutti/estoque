d1b74d14437459cc4ef7ae8f6a0930e038e8f262
David Francisco <david.francisco@vivenda.br>
2018-11-07 14:48:55 -0200

    segundo commit

---
+++ CONTEUDO ESTOQUE/BANCO.sql
@@ -0,0 +1,68 @@
+/*
+Banco de dados criado por: DAVID FRANCISCO DE SANTANA
+VERSÃO ATUAL 0.0.1
+FEITO PARA ARMAZENAR DADOS DO ESTOQUE DA CPA/CD ENQUANTO EU ESTIVER
+
+*/
+
+CREATE DATABASE estoque;
+
+USE estoque;
+
+CREATE TABLE compraProd (
+    id int auto_increment,
+    produto VARCHAR(70) NOT NULL,
+    fornecedo VARCHAR(45) NOT NULL,
+    marca VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    nSerie VARCHAR(60) NOT NULL,
+    dataCompra DATE NOT NULL,
+    valor VARCHAR(15) NOT NULL,
+    quantidade INT(5) NOT NULL,
+    localidade varchar(70),
+primary key(id));
+
+CREATE TABLE consertoProd(
+id int auto_increment,
+produto VARCHAR(70) NOT NULL,
+oficina VARCHAR(45) NOT NULL,
+marca VARCHAR (45) NOT NULL,
+modelo VARCHAR (45) NOT NULL,
+nSerie VARCHAR (60) NOT NULL,
+dataConserto DATE NOT NULL,
+valor VARCHAR(15) NOT NULL,
+quantidade int(5) NOT NULL,
+localidade varchar(70),
+primary key(id));
+
+
+SELECT * FROM compraprod;
+SELECT * FROM consertoprod;
+
+DELETE FROM compraprod WHERE id = 1;
+
+-- TABELAS DO ESTOQUE VERSÃO 0.0.1  TABELA DE COMPRA E CONSERTO --
+
+
+CREATE TABLE utilizadoCompra (
+    produto VARCHAR(70) NOT NULL,
+    fornecedo VARCHAR(45) NOT NULL,
+    marca VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    nSerie VARCHAR(60) NOT NULL,
+    localidade VARCHAR(70)
+);
+
+
+CREATE TABLE USANDO (
+	produto VARCHAR(70) NOT NULL,
+    fornecedo VARCHAR(45) NOT NULL,
+    marca VARCHAR(45) NOT NULL,
+    modelo VARCHAR(45) NOT NULL,
+    nSerie VARCHAR(60) NOT NULL,
+    localidade VARCHAR(70)
+);
+
+select * from usando;
+
+insert into usando values ('hd', 'eu', 'a', 'top', '124621654245154', 'pc david');
---
+++ EQUIPAMENTOS EM USO/em-uso.php
@@ -0,0 +1,47 @@
+<?php 
+//conexão com banco
+
+$login = 'root';
+$pass = '';
+$serve = '127.0.0.1';
+$bd = 'estoque';
+
+$conect = mysqli_connect("127.0.0.1", "root", "", "estoque");
+// fim da conexão 
+?>
+
+<table border="1">
+	<tr>
+		<td>produto</td>
+		<td>fornecedor</td>
+		<td>marca</td>
+		<td>modelo</td>
+		<td>nº serie</td>
+		<td>local</td>
+	</tr>
+
+
+						<?php 
+						
+						$consultardados = "SELECT * FROM usando";
+						$con = mysqli_query($conect, $consultardados);
+
+						while ($mostrar = mysqli_fetch_array($con)) {
+						?><!--começo do while-->
+							
+								<td>	<?php echo $mostrar ['produto']; ?>		</td>
+								<td>	<?php echo $mostrar ['fornecedo']; ?>	</td>
+								<td>	<?php echo $mostrar ['marca']; ?>		</td>
+								<td>	<?php echo $mostrar ['modelo']; ?>		</td>
+								<td>	<?php echo $mostrar ['nSerie']; ?>		</td>
+								<td>	<?php echo $mostrar['localidade']; ?>	</td>
+
+						<?php 
+						}; //fim do while
+						?>
+						</table>
+
+
+<form>
+    <input type="button" value="Voltar" onClick="history.go(-1)">
+</form>
